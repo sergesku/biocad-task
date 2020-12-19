33d4f0a768d9e55575099b7c29d43f48f561e1e5
Sergey Kurgak <kurgak.ss@gmail.com>
2020-12-19 16:22:31 +1100

    Function.putReaction,Types.Direction,Main.samples

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,34 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Types
+import Data.Text
+import Functions
+import Control.Exception
+import Data.Default
+import Database.Bolt
+import Database.Bolt.Extras.Graph
+
+r = Reaction (Name "ReactName")
+m1 = Molecule (Smiles "HOH") (Name "Mol-1-Name")
+m2 = Molecule (Smiles "C-IU") (Name "Mol-2-Name")
+m3 = Molecule (Smiles "H") (Name "Mol-3-Name")
+m4 = Molecule (Smiles "GHDS") (Name "Mol-4-Name")
+
+rd = ReactionData r [m1,m2,m3] [(m4, PRODUCT_FROM (Amount 14))] []
+
+boltCfg :: BoltCfg
+boltCfg = def { host = "localhost"
+              , user = "neo4j"
+              , password = "movie"
+              }
+
+runQueryDB :: BoltActionT IO a -> IO a
+runQueryDB act = bracket (connect boltCfg) close (`run` act)
+
 main :: IO ()
-main = print "start"
+main = do
+  resp <- runQueryDB $ putReactionGraph rd
+  print resp
+  
--- src/Functions.hs
+++ src/Functions.hs
@@ -1,13 +1,49 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ParallelListComp #-}
+{-# LANGUAGE TypeApplications #-}
+
 module Functions where
 
 import Types
+import Data.List (foldl')
+import Data.Text (Text, pack)
+import Data.Function ((&))
 import Database.Bolt
+import Database.Bolt.Extras
+import Database.Bolt.Extras.Graph
 
 putReaction :: ReactionData -> BoltActionT IO ()
-putReaction = undefined
+putReaction = makeRequest @PutRequest [] . putReactionGraph
 
 getReaction :: Id Reaction -> Reaction
 getReaction = undefined
 
 findShortPath :: Molecule -> Molecule -> Path
 findShortPath = undefined
+
+indNames :: NodeName -> [NodeName]
+indNames n = map ((n <>) . pack . show) [1..]
+
+addNodeLike :: NodeLike n => NodeName -> n -> GraphPutRequest -> GraphPutRequest
+addNodeLike name = addNode name . MergeN . toNode
+
+addRelationLike :: URelationLike r => NodeName -> NodeName -> r -> GraphPutRequest -> GraphPutRequest
+addRelationLike from to = addRelation from to . MergeR . toURelation
+
+nodeRelList :: (NodeLike n, URelationLike r) => Direction -> Text -> [(n,r)] -> [GraphPutRequest -> GraphPutRequest]
+nodeRelList dir prefix lst = [ (directed addRelationLike name "reaction" rel) . (addNodeLike name node)
+                             | name <- indNames prefix
+                             | (node, rel) <- lst ]
+                            where 
+                              directed f = case dir of
+                                ToReaction   -> f
+                                FromReaction -> flip f
+                           
+putReactionGraph :: ReactionData -> GraphPutRequest
+putReactionGraph ReactionData{..} = foldl' (&) emptyGraph $ concat [reactionNode, reagentList, productList, catalystList]
+  where
+    reactionNode = [addNodeLike "reaction" rdReaction]
+    reagentList  = nodeRelList ToReaction "reagent" $ zip rdReagents (repeat REAGENT_IN)
+    productList  = nodeRelList FromReaction "product" rdProducts
+    catalystList = nodeRelList ToReaction "catalyst" rdCatalyst
