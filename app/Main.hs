979500865763af8b3ddd4ec9d60ef2b5b3238e2b
Sergey Kurgak <kurgak.ss@gmail.com>
2020-12-24 22:58:05 +1100

    refact Main.main

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,13 @@
 module Main where
 
-import Types
 import SampleData                 (randomReaction)
 import Functions.TextRequest
 
 import Data.Default               (def)
 import Database.Bolt
 import Control.Exception          (bracket)
-import Control.Monad              (replicateM, forM, forM_)
+import Control.Monad              (replicateM, forM_)
+import Control.Monad.IO.Class     (liftIO)
 
 boltCfg :: BoltCfg
 boltCfg = def { host = "localhost"
@@ -21,5 +21,5 @@ runQueryDB act = bracket (connect boltCfg) close (`run` act)
 main :: IO ()
 main = do
   reacts <- replicateM 50 randomReaction
-  ids <- forM reacts $ runQueryDB . putReaction
-  forM_ ids $ \ (Id i) -> print $ "Created reaction: Id " ++ show i
+  runQueryDB $ forM_ reacts $ \r -> do i <- putReaction r
+                                       liftIO $ print $ "Created reaction: Id " ++ show i
